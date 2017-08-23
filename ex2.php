e47494708d8b69c3fbd40086f20642be2d884bcf
aceplu052\ace <ayeayelin@aceplussolutions.com>
2017-08-23 11:58:04 +0630

    add two files

---
+++ ex2.php
@@ -0,0 +1,13 @@
+<?php 
+	$var = 'PHP Tutorial';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $var;?></title>
+</head>
+<body>
+	<h3><?php echo $var;?></h3>
+	<a href="ex1.php"><?php echo $var;?></a>
+</body>
+</html>
