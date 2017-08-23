037965a7f0c5d42d24afdf0ef70cb0ead2096120
aceplu052\ace <ayeayelin@aceplussolutions.com>
2017-08-23 15:52:15 +0630

    chaged ex2.php

---
+++ ex2.php
@@ -0,0 +1,14 @@
+<?php 
+	$var = 'PHP Tutorial';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $var;?></title>
+</head>
+<body>
+	<h3><?php echo $var;?></h3>
+	<a href="ex1.php"><?php echo $var;?></a>
+	<?php echo "testing."?>
+</body>
+</html>
